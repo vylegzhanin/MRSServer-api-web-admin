851eac6ec958a609c6929143871ff017edf12c5e
Alexei Vylegzhanin <alexei.vylegzhanin@gmail.com>
2017-07-16 00:21:50 +0500

    added vaadin-charts (removed vaadin widgetset maven configuration)

---
+++ web-admin/src/main/kotlin/AppWidgetset.kt
@@ -0,0 +1,23 @@
+import com.vaadin.server.WidgetsetInfo
+
+/* Addons in this ws:
+ * 8.0.6
+ * OBF
+ * vaadin-charts:com.vaadin:4.0.0
+
+ */
+class AppWidgetset : WidgetsetInfo {
+
+    override fun getWidgetsetName(): String {
+        return "ws5afd4ba056777a047868f54d37b52a45"
+    }
+
+    override fun getWidgetsetUrl(): String? {
+        return null
+    }
+
+    override fun isCdn(): Boolean {
+        return !true
+    }
+
+}
